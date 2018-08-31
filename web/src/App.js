e4efdd3ed366ec5dc094ed8728a36a3f52d608a8
egenerat <emile.generat@outlook.com>
2018-08-31 21:57:29 +0100

    Display a hardcoded list of ingredients

--- web/src/App.js
+++ web/src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const food = [
+  {
+    'name': 'rice',
+    'status': 'low fodmap'
+  },
+  {
+    'name': 'onion',
+    'status': 'high fodmap'
+  },
+  {
+    'name': 'apple',
+    'status': 'high fodmap'
+  }
+];
+
+const foodItems = food.map((food) =>
+  <li key={food.id}>
+    {food.name}: {food.status}
+  </li>
+);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">fodmap</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ul>{foodItems}</ul>
       </div>
     );
   }
