3b89c354286d8878b98a2d7b020181b3ea160d12
egenerat <emile.generat@outlook.com>
2018-09-09 15:27:45 +0100

    UI improvements: First box model

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 const food = [
@@ -14,14 +14,32 @@ const food = [
   {
     'name': 'apple',
     'status': 'high fodmap'
+  },
+  {
+    'name': 'strawberry',
+    'status': 'low fodmap'
   }
 ];
 
-const foodItems = food.map((food) =>
-  <li key={food.id}>
-    {food.name}: {food.status}
-  </li>
+const foodItemsBoxes = food.map((food) => {
+    const fodmapStyle = food.status === 'high fodmap' ? 'App-high' : 'App-low';
+    const classes = `${fodmapStyle} boxes`;
+    return <div key={food.id} className={classes}>
+      {food.name}
+    </div>
+  }
 );
+
+function UserInput() {
+  return (
+    <form>
+      <label>
+        Ingredient:
+            <input type="text" name="name" />
+      </label>
+    </form>
+  );
+}
 
 class App extends Component {
   render() {
@@ -31,14 +49,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">fodmap</h1>
         </header>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>{foodItems}</ul>
+        <UserInput></UserInput>
+        {foodItemsBoxes}
       </div>
     );
   }
